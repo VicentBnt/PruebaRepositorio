b98dbfa2882dbd4736c4005d972594009cae388f
VicentBnt <vicentbnt03@gmail.com>
2025-05-26 20:28:13 +0200

    main funcionalidad-extra cambio

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
-public class main {
+public class main{
     public static void main(String[] args) {
-        System.out.println("Esto es la rama funcionalidad-extra");
+        System.out.println("Esto es la rama funcionalidad-extra cambiada");
     }
 }
